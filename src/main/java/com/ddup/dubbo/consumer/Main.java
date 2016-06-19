c97b9db4879ca6165f83278d6bedacc4836892e4
ZhangYaxu <alanzhangyx@163.com>
2016-06-19 18:55:57 +0800

    一个调用Dubbo服务dubbo-practice-provider的测试项目。
    1.
    仅有pom.xml和README.md文件，忽略了IDE相关的配置文件，因为要屏蔽不同IDE的差异，使用Eclipse或IntelliJ时能自动配置。
    2. 实际调用的服务提供者是dubbo-practice-provider项目。
    3. 调用com.ddup.dubbo.consumer.Main.main()方法来启动。

---
+++ src/main/java/com/ddup/dubbo/consumer/Main.java
@@ -0,0 +1,23 @@
+package com.ddup.dubbo.consumer;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.ddup.dubbo.service.api.PersonService;
+import com.ddup.dubbo.service.dto.req.PersonDescriptionDTO;
+import com.ddup.dubbo.service.dto.resp.PersonDTO;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring-dubbo.xml");
+		context.start();
+		
+		PersonService personservice = (PersonService)context.getBean("personService");
+		PersonDescriptionDTO dtoReq = new PersonDescriptionDTO();
+		PersonDTO dtoResp = personservice.queryPerson(dtoReq);
+		System.out.println(dtoResp.toString());
+		
+		context.close();
+	}	
+
+}
